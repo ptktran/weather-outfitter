d8a37e73835cef8dcd84ade8f67c9814bb2958e1
Peter Tran <petertranminh22@gmail.com>
2023-05-06 02:44:32 -0400

    basic navbar

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,23 @@
-<nav>
-  <a href="/about">Link 1</a>
-</nav>
+<!-- +page.svelte -->
+<script>
+
+</script>
+
+<html lang="html">
+  <nav class="bg-black w-100 p-1">
+    menu bar
+  </nav>
+  <main>
+    <div>
+      current and future weather
+    </div>
+
+    <div>
+      outfit recommendations
+    </div>
+
+    <div>
+      song recommendation?
+    </div>
+  </main>
+</html>
